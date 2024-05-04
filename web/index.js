12fb4bbf7e615507818d4b7033f9b9d4c08f236e
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-05-04 00:34:46 +0000

    update

--- web/index.js
+++ web/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('coucou je suis ton serveur web')
+  res.send('coucou j suis ton serveur web')
 })
 
 app.listen(port, () => {
