9aad607e55d9fcab6fa3c2aca3890b3084361ff7
Djeradé Golbé Parfait <djeradegolbeparfait@gmail.com>
2024-07-12 11:31:43 +0100

    update

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,49 @@
-import Image from "next/image";
+import { fetchPosts } from "@/db/queries/posts";
+import Link from "next/link";
+import PostDelete from "@/components/post-delete";
+import { Button, Flex , Text} from "@chakra-ui/react";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await fetchPosts()
+
+
+  
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
   return (
-    <main >
-      salut commejhjhjhnt tu vas ?
-    </main>
+    <Flex h={'100vh'} w={'full'} bg={'whitesmoke'}>
+        <Link href="/posts/create" className="bg-white px-4 py-2 rounded">
+        <Text  textColor={'black'} variant=''>
+        Create Post
+        </Text>
+        </Link>
+ 
+      <Flex  w={'full'}>
+      {posts.map(post => {
+          return <Flex key={post.id}>
+            <Flex>
+              <Link
+                key={post.id}
+                href={`/posts/${post.id}/edit`}
+                className=""
+              >
+               <Text textColor={'black'}>
+               {post.title}
+               </Text>
+              </Link>
+              <Text>
+                {post.content}
+              </Text>
+            </Flex>
+            <div className="text-sm opacity-30">{'Updated at ' + post.updatedAt.toLocaleDateString("en-US", dateOptions)}</div>
+            <PostDelete id={post.id} />
+          </Flex>
+        })}
+      </Flex>
+    </Flex>
   );
 }
